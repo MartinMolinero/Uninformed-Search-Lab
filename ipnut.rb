871b29f18c49de203181f05bcc7f24ebafa2198a
MartinMolinero <a01204818@itesm.mx>
2018-02-16 18:37:16 -0600

    stack and node structures

--- ipnut.rb
+++ ipnut.rb
@@ -1,9 +1,48 @@
+class Stack
+	def initialize
+		local = Array.new()
+	end
+	def pop
+		local.pop()
+	end
 
+	def push
+		local.push(local.length)
+	end
+end
+
+
+class Node
+
+	def initialize(stack_arr, parent, action, path_cost)
+		@stacks = stack_arr
+	end
+
+	def calculate_actions
+
+		@stacks.each_with_index do |s|
+
+		end
+	end
+
+	def children
+		#compute the possible children from the action set
+	end
+
+	def parent
+		return @parent
+	end
+
+	def state
+
+	end
+
+end
 def parse
 	max = $stdin.readline
 	start = $stdin.readline.chomp()
 	goal = $stdin.readline.chomp()
-
+	stacks = Stack.new()
 	stacks = start.split(";")
 	goal = goal.split(";")
 
