11c2198a470c8026d5b748e44180a215b755f282
Moises Montano <a01271656@itesm.mx>
2018-02-16 18:33:00 -0600

    finished cost function

--- ipnut.rb
+++ ipnut.rb
@@ -25,4 +25,12 @@ def parse
 	print goal
 end
 
-parse
+# Returns the cost of applying the action (which has the movement)
+# Picking up the container takes 0.5 minutes
+# Moving the container one stack to the left or right takes 1 minute.
+# Putting the container down takes 0.5 minutes
+def cost_function(action)
+	(action[0] - action[1]).abs + 1
+end
+
+print cost_function([2,4])
